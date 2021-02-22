9c5924e429790ef3a863240e315bbcd3befafdc6
tiagoassunc <tiagoassunc@gmail.com>
2021-02-22 15:41:01 -0300

    "Inclusão da pasta '02', matéria Js"

---
+++ Matter/base-02-Js.js
@@ -0,0 +1,71 @@
+/* let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+console.log("Jonas");
+console.log(23);
+
+let fistName = "Tiago";
+console.log(fistName); */
+
+/* true;
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun); */
+
+/* const birthYear = 1991;
+// birthYear = 1990;
+//const job;
+
+// not use more
+var job = "programmer";
+job = "teacher";
+console.log(job) */
+
+
+/* // Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 **3)
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'jonas';
+const lastName = "Schemedtmann";
+console.log(firstName + ' ' + lastName);
+
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++;// x = x + 1
+x--; // x = x - 1 = 100
+x--; // x = x - 1 = 99
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018); */
+
+/* const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018)
+
+let x,y;
+x = y = 25 - 10 -5; // x = y = 10
+console.log(x,y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge); */
