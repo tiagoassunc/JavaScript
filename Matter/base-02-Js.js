66a496fc607a9dd744c6da4af720747494a7e97d
tiagoassunc <tiagoassunc@gmail.com>
2021-02-23 16:59:22 -0300

    "Implementação de conteúdo na pasta de matéria"

--- Matter/base-02-Js.js
+++ Matter/base-02-Js.js
@@ -223,3 +223,69 @@ if(hasDriversLicense && hasGoodVision && !isTired) {
 } else {
   console.log('Some else should drive...')
 } */
+
+
+/* 
+ PART 12
+const day = 'sunday';
+
+switch(day) {
+  case 'monday': //day ==='monday'
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+    break;
+  case 'tuesday':
+    console.log('Prepare theory videos');
+    break;
+  case 'wednesday':
+  case 'thursday':
+    console.log('Write code examples');
+    break;
+  case 'friday':
+    console.log('Record videos');
+    break;
+  case 'saturday':
+  case 'sunday':
+    console.log('Enjoy the weekend :D');
+    break;
+  default:
+    console.log('Not a valid day');
+}
+
+//const day = 'monday';
+
+if(day === 'monday') {
+  console.log('Plan course structure');
+  console.log('Go to coding meetup');
+} else if(day === 'tuesday') {
+  console.log('Prepare theory videos');
+} else if(day === 'wednesday' || day === 'thursday') {
+  console.log('Write code examples');
+} else if(day === 'friday') {
+  console.log('Record videos');
+} else if(day === 'saturday' || day === 'sunday' ) {
+  console.log('Enjoy the weekend :D');
+} else {
+  console.log('Not a valid day');
+} */
+
+
+/* 
+  PART 13 ("The conditional ternary operator")
+const age = 15;
+// age >= 18 ? console.log('I like to drink wine 🍷'):
+//console.log('I like to drink water 💧');
+
+const drink = age>= 18 ? 'wine🍷' : 'water💧';
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+  drink2 = 'wine🍷'
+} else {
+  drink2 ='water💧';
+}
+
+console.log(drink2)
+
+console.log(`I like to drink ${age>= 18 ? 'wine🍷' : 'water💧'}`); */
