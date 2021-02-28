e9fcc3be3497b7d36afe706e543874c331e9eb12
tiagoassunc <tiagoassunc@gmail.com>
2021-02-28 11:48:22 -0300

    "Implementação de conteúdo na pasta de matéria"

--- Matter/base-02-Js.js
+++ Matter/base-02-Js.js
@@ -421,3 +421,74 @@ const yearsUntilRetirement = function (birthYear, firstName) {
 }
 console.log(yearsUntilRetirement(1991, 'Tiago'));
 console.log(yearsUntilRetirement(1970, 'Mike')); */
+
+
+
+/* 
+ Part 20 - Arrays 
+const friend1 = 'Michael';
+const friend2 = 'Steven';
+const friend3 = 'Peter';
+
+const friends = ['Thuz', 'Thales', 'Marcelo'];
+console.log(friends);
+
+//const years = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0], friends[2]);
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = 'Scott';
+console.log(friends);
+// friends = ['Bob', 'Alice']
+
+const fistName = 'Tiago';
+const tiago = [fistName, 'Assunção', 2021 - 2001, 'programmer', friends];
+console.log(tiago)
+
+// Exercise
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+}
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])]
+console.log(ages); */
+
+
+
+// Add elements
+ const friends = ['Michael', 'Steven', 'Peter'];
+ const newLength = friends.push('Tiago');
+ console.log(friends);
+ console.log(newLength);
+
+ friends.unshift('John');
+ console.log(friends);
+
+// Remove elements
+ friends.pop();// Last
+ const popped = friends.pop();
+ console.log(popped);
+ console.log(friends);
+
+ friends.shift();
+ console.log(friends);
+
+ console.log(friends.indexOf('Steven'));
+ console.log(friends.indexOf('Bob'));
+
+ friends.push(23);
+ console.log(friends.includes('Steven'));
+ console.log(friends.includes('Bob'));
+ console.log(friends.includes(23));
+
+ if (friends.includes('Steven')) {
+   console.log('You have a friend called Steven');
+ }
