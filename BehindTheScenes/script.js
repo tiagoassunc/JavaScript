195b7033e1b49c8eb4740cf0e53b06876d1716b8
tiagoassunc <tiagoassunc@gmail.com>
2021-03-16 11:06:52 -0300

    "This keyword na prática"

--- BehindTheScenes/script.js
+++ BehindTheScenes/script.js
@@ -1,8 +1,8 @@
 'use strict';
 
-/*=================== Scope and socpe chain ===================================*/
+//=================== Scope and socpe chain ===================================
 
-function calcAge(birthYear) {
+/* function calcAge(birthYear) {
   const age = 2037 - birthYear;
   // console.log(firstName); // Global variable - Did Variable lookup
 
@@ -36,10 +36,10 @@ const firstName = 'jonas';
 calcAge(1991); // Function called after 'firstName' was declared, so it's okay
 // console.log(age); // Impossible to acess with scope chain
 // printAge();// Impossible to acess with scope chain
+ */
+//========================= Hoisting and TDZ ===================================
 
-/*========================= Hoisting and TDZ ===================================*/
-
-// -Variables
+/* // -Variables
 console.log(me);
 // console.log(job);
 // console.log(year);
@@ -79,4 +79,39 @@ const z = 3;
 
 console.log(x === window.x);
 console.log(y === window.y);
-console.log(z === window.z);
+console.log(z === window.z); */
+
+/*========================= This keyword ===================================*/
+
+/* console.log(this); // Window - this of global scope
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this); // Undefined , function this keyword - because strict mode, without strict mode like 'console.log(this);' show global object
+};
+calcAge(1991);
+
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  console.log(this); // Window because arrow do not have the this keyword, so insted use the this of his parent function/scope
+};
+calcAgeArrow(1980);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this); // In method the this keyword is the object calling the method (jonas.method)
+    console.log(2037 - this.year);
+  },
+};
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge(); // Point to matilda that is the object calling the method
+
+const f = jonas.calcAge; // Possible to do cause function is a value
+f(); // do not work because no one is calling, is just a regular function = undefined, undefined.year = error */
