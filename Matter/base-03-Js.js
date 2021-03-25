4d476d840b787cddef2ca34fea7b32e5bea4c944
Tiago Assunção <tiagoassunc@gmail.com>
2021-03-25 10:27:44 -0300

    "The Rest Pattern and Parameters"

--- Matter/base-03-Js.js
+++ Matter/base-03-Js.js
@@ -40,7 +40,52 @@ const restaurant = {
   orderPasta: function (ing1, ing2, ing3) {
     console.log(`Here is your delicius pasta with ${ing1}, ${ing2}, ${ing3}`);
   },
+
+  orderPizza: function (mainIngredient, ...otherIngridients) {
+    console.log(
+      `The main ingredient chosen was ${mainIngredient} and the add-ones was ${otherIngridients} .`
+    );
+    console.log(mainIngredient);
+    console.log(otherIngridients);
+  },
 };
+
+/*========================== Rest Pattern and Parameters ====================================================*/
+
+/* // SPREAD, because on RIGHT side of =
+const arr = [1, 2, ...[3, 4]];
+
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others); // Shows 1,2, [3,4,5] Putting 3,4,5 in others array
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
+
+// Objects
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+
+// 2) Functions
+const add = function (...numbers) {
+  // Rest
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  console.log(sum);
+}; // Rest parameters
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x); // Spread
+
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach'); // Showing a main ingredient and array of others ingredients
+restaurant.orderPizza('mushrooms'); */
 
 /*========================== The Spred Operator  ====================================================*/
 
