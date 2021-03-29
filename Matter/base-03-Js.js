f4ecee4aec123639452818f4972de9163421e31e
Tiago Assunção <tiagoassunc@gmail.com>
2021-03-29 16:34:08 -0300

    "Optional Chaining"

--- Matter/base-03-Js.js
+++ Matter/base-03-Js.js
@@ -53,8 +53,34 @@ const restaurant = {
     console.log(otherIngridients);
   },
 };
-console.log(restaurant.openingHours);
-/*========================== Enhanced Object Literals ====================================================*/
+
+/*========================== Optional Chaining (?.) ====================================================*/
+
+// IF EXISTS
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon);
+//console.log(restaurant.openingHours.mon.open);
+
+// WHIT optional chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+// Example
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for (const day of days) {
+  //console.log(day);
+  const open = restaurant.openingHours[day]?.open ?? 'closed'; // Using [day] because using variable name as property name
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exists');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exists');
+
+// Arrays
+const user = [{ name: 'Jonas', email: 'hello@jonas.io' }];
+//const user = [];
+console.log(user[0]?.name ?? 'User array empty');
 
 /*========================== Looping Arrays (The for-of loop) ====================================================*/
 
