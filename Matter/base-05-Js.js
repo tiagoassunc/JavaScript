d0146d39196aef1b70f3aaa6484cd4cc33fa6fe2
Tiago Assunção <tiagoassunc@gmail.com>
2021-04-12 14:47:25 -0300

    " Functions Accepting Callback Functions "

--- Matter/base-05-Js.js
+++ Matter/base-05-Js.js
@@ -2,6 +2,58 @@
 
 /* ================================_______________________ fUNCTIONS PART __________________________================================== */
 
+/*========================== Functions Accepting Callback Functions ====================================================*/
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher order function - In this case higher-order = transformer, callback = fn
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`); // Functions have methods and properties, '.name'
+};
+
+transformer('JavaScript is the best!', upperFirstWord); // Just pass the the function value, NOT calling!
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses call-back ALL the time!
+const high5 = function () {
+  console.log('✋🏾');
+};
+
+document.body.addEventListener('click', high5);
+
+// Another Examples
+['Tiago', 'Vittoria', 'Stive'].forEach(high5);
+
+const sum = function (a, b) {
+  console.log(a + b);
+};
+const subtraction = function (a, b) {
+  console.log(a - b);
+};
+const multiplication = function (a, b) {
+  console.log(a * b);
+};
+const division = function (a, b) {
+  console.log(a / b);
+};
+const mathTwoVariables = function (a, b, fn) {
+  fn(a, b);
+};
+mathTwoVariables(4, 5, sum);
+mathTwoVariables(4, 5, subtraction);
+mathTwoVariables(4, 5, multiplication);
+mathTwoVariables(4, 5, division);
+
 /*========================== First-Class and Higher-Order Functions ====================================================*/
 
 /* 
