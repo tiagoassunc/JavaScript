8ecd27c9a9583330851044722403e0afab8eeaa3
Tiago Assunção <tiagoassunc@gmail.com>
2021-05-02 10:13:11 -0300

    "The find Method"

--- Matter/base-06-Js.js
+++ Matter/base-06-Js.js
@@ -1,8 +1,55 @@
 'use strict';
 
+/*========================== The find Method ====================================================*/
+
+/* const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+const firstWithdrwal = movements.find(mov => mov < 0); // find => Just the first element in array that satisfy that condition
+console.log(firstWithdrwal);
+
+const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+console.log(account);
+
+// With for of loop
+for (const acc of accounts) {
+  if (acc.owner === 'Jessica Davis') {
+    console.log(acc);
+  }
+} */
+
 /*========================== The Magic of Chaining Methods ====================================================*/
 
-const eurToUsd = 1.1;
+/* const eurToUsd = 1.1;
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
@@ -16,7 +63,7 @@ const totalDepositsUSD = movements
   //.map(mov => mov * eurToUsd)
   .reduce((acc, mov) => acc + mov, 0);
 
-console.log(totalDepositsUSD);
+console.log(totalDepositsUSD); */
 
 /*========================== The reduce Method ====================================================*/
 
