dd73cfd35cf14eec3774811ef17dc0998454936f
Tiago Assunção <tiagoassunc@gmail.com>
2021-05-04 19:46:23 -0300

    "some and every"

--- Matter/base-06-Js.js
+++ Matter/base-06-Js.js
@@ -1,5 +1,33 @@
 'use strict';
 
+/*========================== some and every ====================================================*/
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+console.log(movements);
+// INCLUDES => check equalities
+console.log(movements.includes(-130));
+
+// SOME => Search for a condition returns true or false
+const anyDeposits = movements.some(mov => mov > 3000);
+console.log(anyDeposits);
+
+//EVERY => all elements comply the condition returns true
+console.log(account4.movements.every(mov => mov > 0));
+console.log(movements.every(mov => mov > 0));
+
+// Separate callback => use to refactoring
+const deposit = mov => mov > 0;
+console.log(movements.some(deposit));
+console.log(movements.every(deposit));
+console.log(movements.filter(deposit));
+
 /*========================== The find Method ====================================================*/
 
 /* const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
