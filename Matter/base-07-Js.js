c6b6b85e38b7fe6bf41c30177ec1973c7eb5df17
Tiago Assunção <tiagoassunc@gmail.com>
2021-05-07 10:51:20 -0300

    "Math and Rounding"

--- Matter/base-07-Js.js
+++ Matter/base-07-Js.js
@@ -1,7 +1,7 @@
 'use strict';
 
 //////////////////////////////// Converting and Checking Numbers ////////////
-console.log(23 === 23.0); // Only one data type for numbers - base 2 => allways decimal
+/* console.log(23 === 23.0); // Only one data type for numbers - base 2 => allways decimal
 
 // Base 10 - 0 to 9. 1/10 = 0.1. 3/10 = 3.33333
 // Binary base 2 - 0 1
@@ -10,7 +10,7 @@ console.log(0.1 + 0.2 === 0.3); // Normal error of Js
 
 // Conversion
 console.log(Number('23'));
-console.log(+'23'); // Type coertion
+console.log(+'23'); // Type coercion
 
 // Parsing
 console.log(Number.parseInt('30px', 10)); // Work cause functions are too an object => Need to begin with number
@@ -34,3 +34,49 @@ console.log(Number.isFinite(20));
 console.log(Number.isInteger(23));
 console.log(Number.isInteger(23.0));
 console.log(Number.isInteger(23 / 0));
+ */
+
+//////////////////////////////// Math and Rounding //////////////////////////////////////////////
+console.log(Math.sqrt(25));
+console.log(25 ** (1 / 2));
+console.log(8 ** (1 / 3));
+
+console.log(Math.max(5, 18, 23, 11, 2));
+console.log(Math.max(5, 18, '23', 11, 2)); // Do type coercion
+console.log(Math.max(5, 18, '23px', 11, 2)); // Do type coercion
+
+console.log(Math.min(5, 18, '23', 11, 2));
+
+// Area of circle
+console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+// Random numbers
+console.log(Math.random()); // Number > 0 && Number <1
+console.log(Math.random() * 6);
+console.log(Math.trunc(Math.random() * 6) + 1); // Removing decimal part and + 1 to 6 appears
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min) + 1) + min;
+// 0...1 => 0...(max - min) => min...max
+console.log(randomInt(10, 20));
+
+// INT
+// Rounding integers
+console.log(Math.trunc(23.3)); // Just positive
+
+// Just int
+console.log(Math.round(23.3));
+console.log(Math.round(24.9));
+
+// Rounding up
+console.log(Math.ceil(23.3));
+console.log(Math.ceil(24.9));
+
+// Rounding down
+console.log(Math.floor(23.3));
+console.log(Math.floor('24.9')); // Positive and negative != trunc
+
+// DECIMAL
+console.log((2.7).toFixed(0));
+console.log((2.7).toFixed(3));
+console.log(+(2.345).toFixed(2)); // + => to convert to number
