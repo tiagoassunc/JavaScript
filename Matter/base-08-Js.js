dd1e8b4ef78fdea322fefe53795be569ca558bc3
Tiago Assunção <tiagoassunc@gmail.com>
2021-05-20 20:11:53 -0300

    "Prototypal Inheritance on Built-In Objects"

--- Matter/base-08-Js.js
+++ Matter/base-08-Js.js
@@ -6,8 +6,8 @@
 // Initializate with Capital letters
 const Person = function (firstName, birthYear) {
   // Instance properties
-  this.firstName = firstName;
-  this.birthYear = birthYear;
+  this.firstName = firstName; // this points -> object itself
+  this.birthYear = birthYear; // this points -> object itself
 
   // Never do this ↓ => cause creat a function to every object created
   // this.calcAge = function () {
@@ -34,7 +34,7 @@ console.log(jay instanceof Person);
 /// Prototypes ///
 
 // All objects creat by the constructor function inherit (acces) all methods and properties
-console.log(Person.prototype);
+console.log(Person.prototype); // All Constructor Functions has a Prototype property (Object)
 Person.prototype.calcAge = function () {
   // Do this -> creat only one copy of this function => that objects can acess (ihnreritance)
   console.log(2021 - this.birthYear);
@@ -45,7 +45,7 @@ tiago.calcAge();
 console.log(tiago.__proto__); // prototype of object
 console.log(tiago.__proto__ === Person.prototype); // prototype of object === prototype property of constructor function
 console.log(Person.prototype.isPrototypeOf(tiago)); // Checking if is a prototype of an object
-console.log(Person.prototype.isPrototypeOf(Person)); // Person.prototype means => Prototype Of Linked Objects
+console.log(Person.prototype.isPrototypeOf(Person)); // Person.prototype means => Prototype Of all Objects created/Linked
 
 // Setting properties
 Person.prototype.species = 'Homo Sapiens';
@@ -54,3 +54,27 @@ console.log(jonas.species, tiago.species);
 // Own property => declared on object itself NOT on Prototype property (ihnreted)  ↓
 console.log(tiago.hasOwnProperty('firstName')); // Checking own property
 console.log(tiago.hasOwnProperty('species')); // Checking own property
+
+/// Prototypal Inheritance on Built-In Objects ///
+
+console.log(tiago.__proto__); // Up in prototype chain => prototype property of Person
+console.log(tiago.__proto__.__proto__); // Up in prototype chain => prototype property of Object
+console.log(tiago.__proto__.__proto__.__proto__); // Up in prototype chain => reaches the top so = null
+console.log(Person.prototype.constructor); // Points back to Person
+
+// Prototype of constructor (Array, Object, fuctions, HTMLHeadingElement etc) = protype of all objects created by that constructor
+const arr = [3, 5, 8, 2, 3, 5, 3, 8, 2, 2]; // new Array === []
+console.log(arr.__proto__); // All methods that we can acces => ihnretance from prototype
+console.log(arr.__proto__ === Array.prototype);
+console.log(arr.__proto__.__proto__);
+// Creating methods on Array.protype
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+console.log(arr.unique());
+
+// Exemplae of a enourmous prototype chain __proto__
+const h1 = document.querySelector('h1');
+console.dir(h1);
+
+console.dir(x => x + 1);
