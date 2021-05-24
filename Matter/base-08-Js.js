c47754af95c0f8daaeac81fb8d0d7ad5ef493650
Tiago Assunção <tiagoassunc@gmail.com>
2021-05-24 09:23:30 -0300

    "Encapsulation: Protected Properties and Methods"

--- Matter/base-08-Js.js
+++ Matter/base-08-Js.js
@@ -327,33 +327,40 @@ jay.introduce();
 jay.calcAge(); */
 
 ///// Another Class Example /////
+///// Encapsulation: Protected Properties and Methods /////
 
 class Account {
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this.pin = pin;
-    this.movements = [];
+    // Protected propertie => convention _
+    this._pin = pin;
+    this._movements = [];
+
     this.locale = navigator.language;
 
     console.log(`thanks for oppen a new account, ${owner}.`);
   }
 
   // Public interface (API)
+  getMovements() {
+    return this._movements;
+  }
+
   deposit(val) {
-    this.movements.push(val);
+    this._movements.push(val);
   }
 
   withdraw(val) {
     this.deposit(-val);
   }
 
-  approveLoan(val) {
+  _approveLoan(val) {
     return true;
   }
 
   requestLoan(val) {
-    if (this.approveLoan(val)) {
+    if (this._approveLoan(val)) {
       this.deposit(val);
       console.log(`Loan approved`);
     }
@@ -367,7 +374,8 @@ console.log(acc1);
 acc1.deposit(650);
 acc1.withdraw(150);
 acc1.requestLoan(1000);
-acc1.approveLoan(1000);
+// acc1.approveLoan(1000);
+console.log(acc1.getMovements());
 
 console.log(acc1);
-console.log(acc1.pin);
+// console.log(acc1.pin);
