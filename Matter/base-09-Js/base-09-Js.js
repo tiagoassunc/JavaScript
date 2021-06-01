8c28cf98b7f573a05401a743df92b2ec0c7ce20a
Tiago Assunção <tiagoassunc@gmail.com>
2021-06-01 11:27:30 -0300

    "Throwing Errors Manually"

--- Matter/base-09-Js/base-09-Js.js
+++ Matter/base-09-Js/base-09-Js.js
@@ -142,25 +142,45 @@ setTimeout(() => {
 //     });
 // };
 
-// Chaining Promises
+// Throwing Errors Manually
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(
+    response => {
+      console.log(response);
+      if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+      return response.json();
+    }
+    // error => alert(error)
+  );
+};
 
-// Handling Rejected Promises
+/* // Handling Rejected Promises
 const getCountryData = function (country) {
   // Country 1
   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
     .then(
-      response => response.json()
+      response => {
+        console.log(response);
+        if (!response.ok)
+          throw new Error(`Country not found (${response.status})`);
+        return response.json();
+      }
       // error => alert(error)
     )
     .then(data => {
       renderCountry(data[0]);
-      const neighbour = data[0].borders[0];
+      // const neighbour = data[0].borders[0];
+      const neighbour = 'dsbjc';
       if (!neighbour) return;
 
       // Country 2
       return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
     })
-    .then(response => response.json())
+    .then(response => {
+      if (!response.ok)
+        throw new Error(`Country not found (${response.status})`);
+      response.json();
+    })
     .then(data => renderCountry(data, 'neighbour'))
     .catch(err => {
       console.error(`${err} 💥💥💥`);
@@ -174,4 +194,37 @@ const getCountryData = function (country) {
 btn.addEventListener('click', function () {
   getCountryData('congo');
 });
-getCountryData('dsfsd');
+// getCountryData('dsfsd'); */
+
+const getCountryData = function (country) {
+  // Country 1
+  getJSON(
+    `https://restcountries.eu/rest/v2/name/${country}`,
+    'Country not found'
+  )
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) throw new Error('No neighbour found');
+
+      // Country 2
+      return getJSON(
+        `https://restcountries.eu/rest/v2/alpha/${neighbour}`,
+        'Country not found'
+      );
+    })
+    .then(data => renderCountry(data, 'neighbour'))
+    .catch(err => {
+      console.error(`${err} 💥💥💥`);
+      renderError(`Something went wrong 💥💥💥 ${err.message}. Try again!`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
+};
+
+btn.addEventListener('click', function () {
+  getCountryData('congo');
+});
+getCountryData('australia');
