ad7afbcfa89ef5f706f92114ebd939fa3f14d121
Tiago Assunção <tiagoassunc@gmail.com>
2021-06-05 08:38:46 -0300

    "Returning Values from Async Functions"

--- Matter/base-09-Js/base-09-Js.js
+++ Matter/base-09-Js/base-09-Js.js
@@ -355,12 +355,10 @@ const getPosition = function () {
 const whereAmI = async function (country) {
   try {
     const pos = await getPosition();
-    console.log(pos);
     const { latitude: lat, longitude: lng } = pos.coords;
 
     // Reverse geocoding
     const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
-    console.log(resGeo);
     if (!resGeo.ok) throw new Error('Problem getting location data');
     const dataGeo = await resGeo.json();
 
@@ -371,22 +369,35 @@ const whereAmI = async function (country) {
     if (!res.ok) throw new Error('Problem getting location country');
     const data = await res.json();
     renderCountry(data[0]);
+
+    return `You are in ${dataGeo.city} ${dataGeo.country}`;
   } catch (err) {
-    ///// Error Handling With try...catch /////
     console.error(`${err} 💥`);
     renderError(`💥 ${err.message} 💥`);
+
+    // Reject promise returned from async function
+    throw err;
   }
 };
-whereAmI();
-// whereAmI();
-// whereAmI();
-console.log("If it's async I'm first to appear");
 
-///// Error Handling With try...catch /////
-/* try {
-  let y = 1;
-  const x = 2;
-  x = 3;
-} catch (err) {
-  alert(err.message);
-} */
+//// Returning Values from Async Functions ////
+
+console.log('1: Will getlocation');
+// const city = whereAmI();
+// console.log(city);
+
+// whereAmI()
+//   .then(city => console.log(`2: ${city}`))
+//   .catch(err => console.error(`2: 💥 ${err.message} 💥`))
+//   .finally(() => console.log('3: Finished getting location'));
+
+(async function () {
+  try {
+    const cityMsg = await whereAmI();
+    console.log(`2: ${cityMsg}`);
+    if (!cityMsg) throw new Error('Problem getting location data');
+  } catch (err) {
+    console.error(`2: 💥 ${err.message} 💥`);
+  }
+  console.log('3: Finished getting location');
+})();
