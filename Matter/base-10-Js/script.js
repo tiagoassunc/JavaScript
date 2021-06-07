a054c05ce3ccd9e9797d97a02250bb6bf5746a10
Tiago Assunção <tiagoassunc@gmail.com>
2021-06-07 13:01:28 -0300

    "The Module Pattern"

--- Matter/base-10-Js/script.js
+++ Matter/base-10-Js/script.js
@@ -1,4 +1,4 @@
-// Importing module
+/* //// Importing module ////
 console.log('Importing module');
 
 // named import
@@ -15,8 +15,39 @@ console.log('Importing module');
 // import add, { addToCart, totalPrice as price, tq }  from './shoppingCart.js'; // avoid mix both
 // console.log(price);
 
-import add, { cart } from './shoppingCart.js';
+import add, { cart, shippingCost } from './shoppingCart.js';
 add('pizza', 2);
 add('bread', 5);
 add('apples', 3);
-console.log(cart);
+console.log(cart); */
+
+//// The Module Pattern ////
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+    );
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
